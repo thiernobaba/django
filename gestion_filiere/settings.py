7643b0cdc127eed91820a78d2d43babda69c7c55
thiernobaba <bababa1542@gmail.com>
2025-06-16 12:37:28 +0000

    Update settings.py

--- gestion_filiere/settings.py
+++ gestion_filiere/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 TEMPLATES = [
     {
